a2ea17780d0535f47b4481ad84bf7bd4a4c36fa4
mario_torres <mario.torres@ing.puc.cl>
2020-11-18 13:02:04 -0300

    CongPred v1.0: eRCNN .py (not the .ipynb): -STImgSeqDataset name changed -Add save losses -Add Image output testing -Names of saved files changed for each type of target

--- cali_data_analisys.py
+++ cali_data_analisys.py
@@ -9,7 +9,7 @@ import time
 import sys
 
 
-class STImgSerieDataset(torch.utils.data.Dataset):
+class STImgSeqDataset(torch.utils.data.Dataset):
     def __init__(self, data_file_name, label_conf='mid', target=3, seq_size=72, image_size=72, data_size=0, pred_window=3,
                  transforms=None):
         self.data = pd.read_csv(data_file_name)
@@ -145,13 +145,14 @@ print(cali_dataset_2015.describe())
 label_conf = 'all'
 target = 2  # target: 0-Flow, 1-Occ, 2-Speed, 3-All
 train_data_file_name = "datasets/california_paper_eRCNN/I5-N-3/2015.csv"
-train_set = STImgSerieDataset(train_data_file_name, label_conf=label_conf, target=target)  # target: 0-Flow, 1-Occ, 2-Speed, 3-All
+train_set = STImgSeqDataset(train_data_file_name, label_conf=label_conf, target=target)  # target: 0-Flow, 1-Occ, 2-Speed, 3-All
 train_set, extra = torch.utils.data.random_split(train_set, [100000, len(train_set) - 100000],
                                                  generator=torch.Generator().manual_seed(5))
 val_test_data_file_name = "datasets/california_paper_eRCNN/I5-N-3/2016.csv"
-val_test_set = STImgSerieDataset(val_test_data_file_name, label_conf=label_conf, target=target)
+val_test_set = STImgSeqDataset(val_test_data_file_name, label_conf=label_conf, target=target)
 valid_set, test_set, extra = torch.utils.data.random_split(val_test_set, [50000, 50000, len(val_test_set) - 100000],
                                                            generator=torch.Generator().manual_seed(5))
+
 print(f"Size of train_set = {len(train_set)}")
 print(f"Size of valid_set = {len(valid_set)}")
 print(f"Size of test_set = {len(test_set)}")
@@ -192,7 +193,7 @@ criterion2 = nn.L1Loss()
 optimizer = optim.Adam(e_rcnn.parameters(), lr=1e-3)  # ADAM with lr=10^-4
 scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.1)  # exponential decay every epoch = 2000iter
 
-# sys.stdout = open("log.txt", "w")
+#sys.stdout = open("log.txt", "w")
 torch.cuda.empty_cache()
 loss_plot_train = []
 loss_plot_test = []
@@ -247,7 +248,7 @@ for epoch in range(10):  # 10 epochs
             losses = []
             start = time.time()
     scheduler.step()
-    torch.save(e_rcnn.state_dict(), "state_dict_model.pt")
+    torch.save(e_rcnn.state_dict(), f"eRCNN_state_dict_model_{target}.pt")
 
     # Evaluate
     e_rcnn.eval()
@@ -258,8 +259,8 @@ for epoch in range(10):  # 10 epochs
         for batch_idx, (inputs_test, targets_test) in enumerate(valid_loader):
             inputs_test = inputs_test.permute(1, 0, 2, 3, 4)
             targets_test = targets_test.permute(1, 0, 2)
-            targets_test = targets_test[:, :, 2]
-            targets_test = torch.unsqueeze(targets_test, 2)
+            #targets_test = targets_test[:, :, 2]
+            #targets_test = torch.unsqueeze(targets_test, 2)
             inputs_test, targets_test = inputs_test.to(device), targets_test.to(device)
 
             error_test = e_rcnn.initError(batch_size)
@@ -268,7 +269,7 @@ for epoch in range(10):  # 10 epochs
             for i in range(inputs_test.shape[0]):
                 outputs_test = e_rcnn(inputs_test[i], error_test.detach())
                 err_i = outputs_test - targets_test[i]
-                error_test = torch.cat((error_test[:, 1:], err_i), 1)
+                error_test = torch.cat((error_test[:, detectors_pred:], err_i), 1)
 
             loss = criterion(outputs_test, targets_test[i])
             loss2 = criterion2(outputs_test, targets_test[i])
@@ -286,6 +287,14 @@ for epoch in range(10):  # 10 epochs
     print('--------------------------------------------------------------')
     e_rcnn.train()
 
+with open(f'loss_plot_test_{target}.txt', 'w') as filehandle:
+    for listitem in loss_plot_test:
+        filehandle.write('%s\n' % listitem)
+
+with open(f'loss_plot_test2_{target}.txt', 'w') as filehandle:
+    for listitem in loss_plot_test2:
+        filehandle.write('%s\n' % listitem)
+
 # Plot the training and testing loss
 plt.figure(1)
 plt.plot(loss_plot_train)
@@ -293,25 +302,116 @@ plt.title("Training MSE")
 plt.ylabel("MSE")
 plt.xlabel("Bacthx100")
 plt.grid()
-plt.savefig("train_mse.png")
+plt.savefig(f"train_mse_{target}.png")
 
 flatList_test1 = [item for elem in loss_plot_test for item in elem]
-plt.figure(2)
+plt.figure(20)
 plt.plot(flatList_test1)
+plt.ylim(0, 40)
 plt.title("Testing MSE")
 plt.ylabel("MSE")
-plt.xlabel("Bacthx100")
+plt.xlabel("Bacth")
 plt.grid()
-plt.savefig("test_mse.png")
+plt.savefig(f"test_mse_{target}.png")
 
 flatList_test2 = [item for elem in loss_plot_test2 for item in elem]
 plt.figure(3)
 plt.plot(flatList_test2)
+plt.ylim(0, 3)
+plt.title("Testing MAE")
+plt.ylabel("MAE")
+plt.xlabel("Bacth")
+plt.grid()
+plt.savefig(f"test_mae_{target}.png")
+
+flatList_mean_test1 = [np.mean(elem) for elem in loss_plot_test]
+plt.figure(4)
+plt.plot(flatList_mean_test1)
+plt.ylim(0, 10)
+plt.title("Testing MSE")
+plt.ylabel("MSE")
+plt.xlabel("Bacthx100")
+plt.grid()
+plt.savefig(f"test_mean_mse_{target}.png")
+
+flatList_mean_test2 = [np.mean(elem) for elem in loss_plot_test2]
+plt.figure(5)
+plt.plot(flatList_mean_test2)
+plt.ylim(0, 2)
 plt.title("Testing MAE")
 plt.ylabel("MAE")
 plt.xlabel("Bacthx100")
 plt.grid()
-plt.savefig("test_mae.png")
+plt.savefig(f"test_mean_mae_{target}.png")
+
 plt.show()
 
-# sys.stdout.close()
+
+
+#%%
+# Image Testing
+img_test_set = STImgSeqDataset(val_test_data_file_name, label_conf=label_conf, target=target, seq_size=72*3)
+img_test_set, extra = torch.utils.data.random_split(img_test_set, [100000, len(img_test_set) - 100000],
+                                                 generator=torch.Generator().manual_seed(5))
+e_rcnn.eval()
+total = 0
+losses_test_oneshot = []
+losses_test_oneshot2 = []
+outputs_test_oneshot = []
+targets_test_oneshot = []
+with torch.no_grad():
+    np.random.seed(seed=25)
+    inputs_test, targets_test = img_test_set[np.random.randint(100000)]
+    inputs_test = inputs_test.unsqueeze_(1)
+    targets_test = targets_test.unsqueeze_(1)
+    inputs_test, targets_test = inputs_test.to(device), targets_test.to(device)
+
+    error_test = e_rcnn.initError(1)
+    error_test = error_test.to(device)
+    loss = torch.zeros(1, requires_grad=True)
+    for i in range(inputs_test.shape[0]):
+        outputs_test = e_rcnn(inputs_test[i], error_test.detach())
+        loss = criterion(outputs_test, targets_test[i])
+        loss2 = criterion2(outputs_test, targets_test[i])
+        losses_test_oneshot.append(loss.item())
+        losses_test_oneshot2.append(loss2.item())
+        err_i = outputs_test - targets_test[i]
+        error_test = torch.cat((error_test[:, detectors_pred:], err_i), 1)
+        if i > 40:
+            outputs_test_oneshot.append(outputs_test)
+            targets_test_oneshot.append(targets_test[i])
+outputs_test_oneshot = torch.cat(outputs_test_oneshot)
+targets_test_oneshot = torch.cat(targets_test_oneshot)
+outputs_test_oneshot = outputs_test_oneshot.permute(1, 0)
+targets_test_oneshot = targets_test_oneshot.permute(1, 0)
+#print(outputs_test_oneshot)
+#print(targets_test_oneshot)
+#img_out = (outputs_test_oneshot/ outputs_test_oneshot.max())
+#img_target = (targets_test_oneshot/ outputs_test_oneshot.max())
+plt.figure()
+plt.imshow(outputs_test_oneshot.cpu())
+plt.savefig("img_out.png")
+plt.show()
+plt.figure()
+plt.imshow(targets_test_oneshot.cpu())
+plt.savefig(f"img_target_{target}.png")
+plt.show()
+
+plt.figure()
+plt.plot(losses_test_oneshot)
+plt.title("Training MSE")
+plt.ylabel("MSE")
+plt.xlabel("Bacthx100")
+plt.grid()
+plt.savefig(f"train_mse_oneshot_{target}.png")
+
+plt.figure()
+plt.plot(losses_test_oneshot2)
+#plt.ylim(0, 3)
+plt.title("Testing MAE")
+plt.ylabel("MAE")
+plt.xlabel("Bacth")
+plt.grid()
+plt.savefig(f"test_mae_oneshot_{target}.png")
+
+#sys.stdout.close()
